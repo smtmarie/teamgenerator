e3e70b2cac354819e67a67f0c7267dcafc722e3f
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-04-20 16:13:51 -0600

    still building and rebuilding

--- app.js
+++ app.js
@@ -11,12 +11,190 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
+
+const fullTeam = [];
+
+
+function managerDetails() {
+
+    inquirer.prompt([
+
+        {
+
+            type: "input",
+            name: "name",
+            message: "Please enter your name.",
+
+        },
+
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter your ID number.",
+
+        },
+
+        {
+
+            type: "input",
+            name: "email",
+            message: "Please enter your email address.",
+
+        },
+
+        {
+
+            type: "input",
+            name: "officeNumber",
+            message: "Please enter office number.",
+
+        }
+
+    ]).then(function (answers) {
+
+        const manager = newManager(answers.type, answers.name, answers.email, answers.officeNumber);
+        fullTeam.push(manager);
+        createTeam();
+
+    })
+}
+
+function createTeam() {
+
+    inquirer.prompt([
+
+        {
+
+            type: "input",
+            name: "role",
+            message: "Please enter your role.",
+            option: ["Engineer", "Intern", "Manager"]
+
+        }
+
+    ]).then(function (answer) {
+
+        switch (answer.option) {
+
+            case "Engineer":
+                engineerDetails()
+
+                break;
+
+            case "Intern":
+                internDetails()
+
+                break;
+
+            default: completeTeam()
+
+        }
+    })
+}
+
+function engineerDetails() {
+
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "Please enter your name.",
+            message: "Please enter office number.",
+
+        },
+
+        {
+
+            type: "input",
+            name: "id",
+            message: "Please enter your ID number."
+
+        },
+
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email address.",
+
+        },
+
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter your Github username.",
+
+        },
+
+
+    ]).then(function (answers) {
+
+        const engineer = newEngineer(answers.type, answers.name, answers.id, answers.email, answers.github);
+        fullTeam.push(engineer)
+        createTeam()
+
+    })
+}
+
+function internDetails() {
+
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter your name.",
+
+        },
+
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter your ID number.",
+
+        },
+
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email address.",
+
+        },
+
+        {
+            type: "input",
+            name: "school",
+            message: "Please enter the name of your school.",
+
+        },
+
+    ]).then(function(answers) {
+
+        const intern = newIntern(answers.type, answers.name, answers.id, answers.email, answers.school)
+        fullTeam.push(intern)
+        createTeam()
+
+    })
+}
+
+function completeTeam() {
+
+    if(!fs.existsSync(OUTPUT_DIR)) {
+
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+
+    fs.writeFileSync(outputPath, render(fullTeam), "utf-8")
+}
+
+managerDetails();
+
+
+
+
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
-// const allEmployees = [];
-
-// function managerDetails ()
 
 
 // After the user has input all employees desired, call the `render` function (required
@@ -26,23 +204,9 @@ const render = require("./lib/htmlRenderer");
 
 
 
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-
-
-
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-
-
 
 
 
